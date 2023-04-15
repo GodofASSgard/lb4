911c17ccdc63e34e168da377790ebcce8e158701
GodofASSgard <boevayanata2002@gmail.com>
2023-04-15 16:12:53 +0700

    first commit

---
+++ src/inficecream.java
@@ -0,0 +1,10 @@
+public class inficecream {
+
+    public static final String[] Coffe = new String[] {
+            "Шоколадное" , "Ванильное", "Клубничное"
+    };
+
+    public static final String[] Info = new String[]{
+            " Стоимость - 100 rub\n Объем - 1 шарик", " Стоимость - 200 rub\n 2 шарика", " Стоимость - 300 rub\n 3 шарика"
+    };
+}
